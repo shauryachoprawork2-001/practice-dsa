dd9bb6463a4e0acffe352637b723eb4ac22363fe
Shaurya <shauryachoprawork2@gmail.com>
2026-01-31 16:03:44 +0530

    Add variables1 main method

--- variables.java
+++ variables.java
@@ -1,4 +1,9 @@
 public class variables {
- int a=1;
- 
+ public static void main(String[] args) {
+    System.out.println("Hello World");
+ }
+
 }
+// System.out.println this is the function for next line 
+//"hello workd is the output"
+//we could have also used print for same line now to practice we could print starts
---
+++ variables1.java
@@ -0,0 +1,7 @@
+public class variables1 {
+ public static void main(String[] args) {
+     System.err.println("****");
+     System.err.println("***");
+     System.err.println("**");
+ }
+}
