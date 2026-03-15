06461b2f0d0f53f0ad842cc63c8930de79af7fe8
Shaurya <shauryachoprawork2@gmail.com>
2026-03-15 20:48:04 +0530

    hey

--- practicedsa1.java
+++ practicedsa1.java
@@ -1,15 +1,11 @@
 
 public class practicedsa1{
     public static void ra(int arr[]){
-       int start =0;
-       int end =arr.length-1;
-       while(start<=end){
-        int temp=arr[start];
-         arr[start] =arr[end];
-         arr[end]=temp;
-         start++;
-         end--;
-       } 
+      for(int i=0;i<arr.length-1;i++){
+        int j=i+1;
+        System.err.println(i+","+j);
+      }
+       
         }
     
        
@@ -17,16 +13,14 @@ public class practicedsa1{
     public static void main(String[] args) {
         int arr[]={2,3,4,5,6,7};
         ra(arr);
-        for (int i = 0; i <= arr.length-1; i++) {
-            System.err.print(arr[i]+" ");
-            
+        
         }
        
        
     }
-}
 
-//pairs in an array
+
+
 //subarrays
 //max sum 2
 //trapping rain water
