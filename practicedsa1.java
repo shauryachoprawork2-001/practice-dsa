00a5ec9c9d71108be393498b2fbb9e73d614a18d
Shaurya <shauryachoprawork2@gmail.com>
2026-03-15 20:05:56 +0530

    hey

--- practicedsa1.java
+++ practicedsa1.java
@@ -1,17 +1,38 @@
+
 public class practicedsa1{
-    public static void largest(int arr[]){
-        int larg= Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>larg){
-               larg=arr[i];
+    public static void binarySearch(int arr[],int key){
+       
+        int start =0;
+        int end=arr.length-1;
+       
+  
+        while(start<=end){
+            int mid=start +((end-start)/2);
+            if(arr[mid]>key ){
+                end=mid-1;
+            }else if (arr[mid]<key){
+                start=mid+1;
 
+            }else if (arr[mid]==key){
+                System.err.println(mid
+                );
+                return;
+            }else{
+                System.err.println("not found");
+                return;
             }
-            
-        }System.out.println(larg+" this is the final part ");
-
+        } System.err.println("not found");
+       
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6};
-        largest(arr);
+        int arr[]={2,3,4,5,6};
+        binarySearch(arr,6);
+       
     }
 }
+//binary seearch
+//reverse an arrya
+//pairs in an array
+//subarrays
+//max sum 2
+//trapping rain water
